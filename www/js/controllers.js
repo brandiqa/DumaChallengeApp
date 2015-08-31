b74fd50027151d071922b4e7d80a8cdf1709a9d5
Michael Wanyoike <mike@brandiqa.com>
2015-08-31 10:30:59 +0300

    Refactored Page Header (moved to index.html)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,11 +4,13 @@
 
 var dumaControllers = angular.module('dumaApp.controllers',[]);
 
-dumaControllers.controller('SignupCtrl',['$scope',
-	function($scope) {
+dumaControllers.controller('SignupCtrl',['$scope','$rootScope',
+	function($scope, $rootScope) {
+		$rootScope.title = 'Employer Signup';
+		$rootScope.sub_title = 'Free Account';
 		$scope.user = {};
 		$scope.createEmployerAccount = function(user) {
-			console.log(user);
+			console.log(angular.toJson(user));
 		}
 	}]
 );
